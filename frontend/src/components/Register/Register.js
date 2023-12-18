addd522c7a797ec6cfca1b700d0c15c19da2336d
ashaydharmik <ashaydharmik807@gmail.com>
2023-12-18 13:08:01 +0530

    added frontend

---
+++ frontend/src/components/Register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./register.scss"
+
+
+
+const Register = () => {
+  return (
+    <section className='register-container'>
+      <div className='left'>  
+      <div className='left-content'>
+        <h1></h1>
+        <p></p>
+      </div>
+      </div>
+      <div className='right'>
+      </div>
+    </section>
+  )
+}
+
+export default Register
