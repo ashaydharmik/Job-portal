fa846d67a1493d10911603fafa8d6ae1c46d2bf6
ashaydharmik <ashaydharmik807@gmail.com>
2024-01-10 18:09:55 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const cors = require("cors")
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}))
 
 
 
