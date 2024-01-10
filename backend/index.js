3c0f77e18a07be3c055083cda2af181d99c0d2f3
ashaydharmik <ashaydharmik807@gmail.com>
2024-01-10 19:04:49 +0530

    created build folder

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,8 @@ const cors = require("cors")
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}))
+app.use(cors())
 
-app.use("/test",(req,res)=>{
-    res.send("server running")
-})
 
 app.use(express.json())
 app.use(bodyParser.json())
