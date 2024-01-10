9a53ca314fe003e70a03e9f93bc654cf0b55604b
ashaydharmik <ashaydharmik807@gmail.com>
2024-01-10 16:03:45 +0530

    up

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use("/",(req,res)=>{
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
+app.options('*', cors());
 app.use("/", require("./routes/userRoute"))
 
 
